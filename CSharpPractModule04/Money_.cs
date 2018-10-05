6325de688a34f218259aa76c2500a748c262add8
NajaNaja89 <najanaja89@gmail.com>
2018-10-05 16:09:20 +0600

    first commit

---
+++ CSharpPractModule04/Money_.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule04
+{
+    public class Money_
+    {
+        private double valueMoney;
+        private double countOfMoney;
+
+        public Money_()
+        {
+            valueMoney = 1.0;
+            countOfMoney = 1.0;
+        }
+
+        public Money_(double valueMoney, double countOfMoney)
+        {
+            this.countOfMoney = countOfMoney;
+            this.valueMoney = valueMoney;
+        }
+
+        public double GetCountOfMoney()
+        {
+            return countOfMoney;
+        }
+        public double GetValueOfMoney()
+        {
+            return valueMoney;
+        }
+
+        public void SetCountOfMoney(double countOfMoney)
+        {
+            this.countOfMoney = countOfMoney;
+        }
+        public void SetValueOfMoney(double valueMoney)
+        {
+            this.valueMoney = valueMoney;
+        }
+
+        public double GetSumOfMoney()
+        {
+            return valueMoney * countOfMoney;
+        }
+
+
+
+        bool IsEnoughtMoney(double sumOfMoney)
+        {
+            if ((valueMoney * countOfMoney) <= sumOfMoney) return true;
+            else return false;
+        }
+
+        double CountOfItem(double costOfItem)
+        {
+            return (valueMoney * countOfMoney) / costOfItem;
+        }
+
+    }
+}
---
+++ CSharpPractModule04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money_ m1;
+            Console.Write("Enter SetCountOfMoney ");
+            m1.SetCountOfMoney(Console.Read());
+            Console.WriteLine();
+            Console.Write("Enter SetValueOfMoney ");
+            m1.SetValueOfMoney(Console.Read());
+            Console.WriteLine();
+            Console.WriteLine(m1.GetCountOfMoney());
+            Console.WriteLine(m1.GetValueOfMoney());
+            Console.WriteLine(m1.GetSumOfMoney());
+ 
+
+        }
+    }
+}
